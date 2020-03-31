75dfc01b2fb03d640987031742e826ee49517882
Kourtnie Henault <kourtnie.m.jenkins@exxonmobil.com>
2020-03-31 08:13:50 -0500

    Upload working project

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstDieImageView: UIImageView!
+    @IBOutlet weak var secondDieImageView: UIImageView!
+    let initialValue = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        firstDieImageView.image = UIImage.init(named: "Dice\(initialValue)")
+        secondDieImageView.image = UIImage.init(named: "Dice\(initialValue)")
+        
+    }
+    
+    @IBAction func rollButtonTapped(_ sender: Any) {
+        rollDice()
+    }
+    
+    
+    func rollDice(){
+        let firstDieRoll = Int.random(in: 1...6)
+        let secondDieRoll = Int.random(in: 1...6)
+        
+        firstDieImageView.image = UIImage.init(named: "Dice\(firstDieRoll)")
+        secondDieImageView.image = UIImage.init(named: "Dice\(secondDieRoll)")
     }
 
 
